1262d1db2a453935da010517509cc0cc70b16c44
saksham.s <sakshams982@gmail.com>
2024-07-02 11:31:02 +0545

    created a config for registration form

---
+++ Dynamic-Form/src/app/constants/registration-form.constants.ts
@@ -0,0 +1,88 @@
+export const registerFormConfig={
+  formTitle:"Registration",
+  saveBtn:"Register",
+  cancelBtn:"Cancel",
+  formControls:[
+    {
+      'name':"firstName",
+      'label':'First Name',
+      'placeholder':"Enter your Name",
+      'value':'',
+      'class':"col-md-6",
+      'type':'text',
+      'validators':[
+        {'validatorName':'required',
+        'required':true,
+        'message':'first name is required'}
+      ]
+    },
+    {
+      'name':"lastName",
+      'label':'Last Name',
+      'placeholder':"Enter your Name",
+      'value':'',
+      'class':"col-md-6",
+      'type':'text',
+      'validators':[
+        {'validatorName':'required',
+          'required':true,
+          'message':'Last name is required'}
+      ]
+    },
+    {
+      'name':"email",
+      'label':'Email',
+      'placeholder':"Enter your email",
+      'value':'',
+      'class':"col-md-6",
+      'type':'email',
+      'validators':[
+        {'validatorName':'required',
+          'required':true,
+          'message':'email is required'},
+        {
+          'validatorName':'email',
+          'email':true,
+          'message':'email is not valid'
+        }
+      ]
+    },
+    {
+      'name':"gender",
+      'label':'Gender',
+      'placeholder':"Choose your gender",
+      'radioOptions':[
+        'Male',
+        'Female'
+      ],
+      'class':"col-md-6",
+      'type':'radio',
+      'validators':[
+        {'validatorName':'required',
+          'required':true,
+          'message':'Selection is required'},
+      ]},
+    {
+      'name':"role",
+      'label':'Role',
+      'placeholder':"Choose your role",
+      'options':[{
+        'id':1,
+        'value':'Cook'
+      },{
+        'id':2,
+        'value':'Planner'
+      },{
+        'id':3,
+        'value':'Enthusiast'
+      }
+      ],
+      'class':"col-md-6",
+      'type':'select',
+      'validators':[
+        {'validatorName':'required',
+          'required':true,
+          'message':'Selection is required'},
+      ]},
+  ]
+}
